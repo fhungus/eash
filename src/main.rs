c4619d8671c7c97807726445623171e04e8653fb
fhungus <wenomech@inasu.ma>
2025-07-23 01:50:40 +0100

    nvm im too fucking dumb to use git properly, here are the fuckass springs!

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use crossterm::{
     cursor::MoveToColumn,
-    event::{self, Event, KeyCode, KeyEvent, KeyModifiers},
+    event::{self, DisableBracketedPaste, Event, KeyCode, KeyEvent, KeyModifiers},
     queue,
     style::{
         Color as ctColor, PrintStyledContent, ResetColor, SetBackgroundColor, SetForegroundColor,
         StyledContent, Stylize,
     },
-    terminal::{disable_raw_mode, enable_raw_mode},
+    terminal::{self, Clear, ClearType, disable_raw_mode, enable_raw_mode},
 };
 
 use std::{
@@ -99,45 +99,77 @@ impl Element {
     }
 }
 
-struct ChainSpring {
+struct ChainMass {
+    mass: f32,
     position: f32,
-    goal_position: f32,
-    dampening: f32,
     velocity: f32,
 }
 
 struct ChainLink {
-    spring: ChainSpring,
+    mass: ChainMass,
     element: Element,
+}
+
+fn calculate_spring_distance(l: &ChainLink, r: &ChainLink) -> u32 {
+    // spring distance should be size of both of its neighbours / 2 + the spacing between them
+    const SPACING: u32 = 2;
+    return ((l.element.get_width() + r.element.get_width()) / 2) + SPACING;
 }
 
 type Chain = Vec<ChainLink>;
 
-fn step_links(chain: &mut Chain) {
-    let mut i = 0;
+// THERE IS SOMETHING HORRIFICALLY WRONG WITH MY SPRING SIMULATION
+const SPRING_CONSTANT: f32 = 0.3;
+const SPRING_DAMPING: f32 = 0.8;
 
-    // collisions
-    let velocities: Vec<f32> = vec![];
+fn calculate_force(chain: &Chain, link_index: usize) -> f32 {
+    let link = &chain[link_index];
+    let mut force: f32 = 0.0;
 
-    for link in chain.iter() {
-        let could = if link.spring.velocity > 0 as f32 {
-            chain.get(i + 1)
-        } else {
-            chain.get(i - 1)
-        };
-
-        let neighbour = match could {
-            Some(n) => n,
-            None => continue,
-        };
-
-        if link.spring.velocity > 0 as f32 {
-            link.spring.
-        } else {
-
+    // left spring
+    // we could DRY this out, right?
+    if link_index != 0 {
+        if let Some(left_neighbour) = chain.get(link_index - 1) {
+            let natural_distance = calculate_spring_distance(left_neighbour, link);
+            let displacement = link.mass.position - left_neighbour.mass.position;
+            force += -SPRING_CONSTANT * (displacement - (natural_distance as f32));
         }
+    } else {
+        // nudge the starting element to zero, so we can anchor to something
+        let natural_distance = link.element.get_width() / 2 + 2;
+        let displacement = link.mass.position; // goal position is ZERO!
+        force += -SPRING_CONSTANT * (displacement - (natural_distance as f32))
+    }
 
-        i = i + 1;
+    // right spring
+    if let Some(right_neighbour) = chain.get(link_index + 1) {
+        let natural_distance = calculate_spring_distance(link, right_neighbour);
+        let displacement = right_neighbour.mass.position - link.mass.position;
+        force += SPRING_CONSTANT * (displacement - (natural_distance as f32));
+    }
+
+    force -= SPRING_DAMPING * link.mass.velocity;
+
+    return force;
+}
+
+fn step_links(chain: &mut MutexGuard<Chain>, dt: f32) {
+    // I DONT KNOW WHAT VERLET INTEGRATION IS
+    let n = chain.len();
+
+    // calculate extra forces and such
+    let mut extra_forces = vec![0.0; n];
+    for i in 0..n {
+        extra_forces[i] = calculate_force(chain, i) / chain.get(i).unwrap().mass.mass;
+    }
+
+    for i in 0..n {
+        let link = chain.get_mut(i).unwrap();
+
+        link.mass.position =
+            link.mass.position + (link.mass.velocity * dt + 0.5 * extra_forces[i] * dt * dt);
+
+        link.mass.velocity += extra_forces[i];
     }
 }
 
@@ -187,18 +219,25 @@ fn process_print_width_as_min(
     return print_content.to_string();
 }
 
-fn render<'a, W>(w: &mut W, elements: &MutexGuard<Vec<Element>>)
+fn render<'a, W>(w: &mut W, elements: &MutexGuard<Chain>)
 where
     W: std::io::Write,
 {
-    _ = queue!(w, MoveToColumn(0));
+    _ = queue!(w, MoveToColumn(0), Clear(ClearType::CurrentLine));
 
-    let mut e = 1;
-    for element in elements.iter() {
-        if e != 1 {
-            _ = queue!(w, ResetColor);
-            print!(" ");
-        }
+    for item in elements.iter() {
+        let element = &item.element;
+
+        let start_position: i32 =
+            item.mass.position.round() as i32 - (element.get_width() as i32 / 2);
+
+        let terminal_position = if start_position >= 0 {
+            start_position
+        } else {
+            0
+        };
+        _ = queue!(w, ResetColor, MoveToColumn(terminal_position as u16));
+
         let vs = &element.state;
         let mut print_content = element.content.clone();
 
@@ -218,8 +257,13 @@ where
 
         // We shouldn't NEED to do this if both color and background_color are solid, which they usually are.
         // TODO: just print everything at once if both colors are solid
-        let mut i = 1;
+        let mut i = 0;
         for char in print_content.chars() {
+            i = i + 1;
+            if i + start_position < 0 {
+                continue;
+            }
+
             let distance = i as f32 / print_content.len() as f32;
             let styled_character = char
                 .to_string()
@@ -227,11 +271,8 @@ where
                 .on(vs.bg_color.to_color_for_char(distance));
 
             _ = queue!(w, PrintStyledContent(styled_character));
-            i = i + 1;
         }
         _ = w.flush();
-
-        e = e + 1;
     }
 }
 
@@ -251,64 +292,82 @@ fn read_ct_keypress_event(event_result: std::io::Result<Event>) -> Option<KeyEve
     return event_result.unwrap().as_key_event();
 }
 
-fn render_thread(element_mutex: Arc<Mutex<Vec<Element>>>) {
-    thread::spawn(move || {
-        let mut stdout = std::io::stdout();
+fn render_thread(element_mutex: Arc<Mutex<Chain>>) {
+    thread::Builder::new()
+        .name("Rendering".to_string())
+        .spawn(move || {
+            let mut stdout = std::io::stdout();
 
-        loop {
-            thread::sleep(Duration::from_millis(1000 / 60)); // i hate this
-            let lock = element_mutex.lock().unwrap();
-            render(&mut stdout, &lock);
-        }
-    });
+            loop {
+                thread::sleep(Duration::from_millis(1000 / 60)); // i hate this
+                let mut lock = element_mutex.lock().unwrap();
+                step_links(&mut lock, 1.0);
+                render(&mut stdout, &lock);
+            }
+        })
+        .expect("erm.... what the thread?");
 }
 
 fn main() {
     let elements = Arc::new(Mutex::new(vec![
-        Element {
-            content: "Garish ass gradients".to_string(),
-            state: VisualState {
-                align: Alignment::Right,
-                width: Width::Minimum(0),
-                padding: 2,
-                bg_color: Color::Gradient(
-                    HexColor {
-                        r: 111,
-                        g: 30,
-                        b: 70,
-                    },
-                    HexColor {
-                        r: 46,
-                        g: 39,
-                        b: 98,
-                    },
-                ),
-                color: Color::Gradient(
-                    HexColor {
-                        r: 244,
-                        g: 209,
-                        b: 76,
-                    },
-                    HexColor {
-                        r: 109,
-                        g: 234,
-                        b: 98,
-                    },
-                ),
+        ChainLink {
+            mass: ChainMass {
+                mass: 0.5,
+                position: 0.0,
+                velocity: 0.0,
+            },
+            element: Element {
+                content: "Garish ass gradients".to_string(),
+                state: VisualState {
+                    align: Alignment::Right,
+                    width: Width::Minimum(0),
+                    padding: 2,
+                    bg_color: Color::Gradient(
+                        HexColor {
+                            r: 111,
+                            g: 30,
+                            b: 70,
+                        },
+                        HexColor {
+                            r: 46,
+                            g: 39,
+                            b: 98,
+                        },
+                    ),
+                    color: Color::Gradient(
+                        HexColor {
+                            r: 244,
+                            g: 209,
+                            b: 76,
+                        },
+                        HexColor {
+                            r: 109,
+                            g: 234,
+                            b: 98,
+                        },
+                    ),
+                },
             },
         },
-        Element {
-            content: "textiticus".to_string(),
-            state: VisualState {
-                align: Alignment::Left,
-                width: Width::Minimum(0),
-                padding: 2,
-                bg_color: Color::Transparent,
-                color: Color::Solid(HexColor {
-                    r: 255,
-                    g: 255,
-                    b: 255,
-                }),
+        ChainLink {
+            mass: ChainMass {
+                mass: 1.0,
+                position: 0.0,
+                velocity: 0.0,
+            },
+            element: Element {
+                content: "textiticus".to_string(),
+                state: VisualState {
+                    align: Alignment::Left,
+                    width: Width::Minimum(0),
+                    padding: 2,
+                    bg_color: Color::Transparent,
+                    color: Color::Solid(HexColor {
+                        r: 255,
+                        g: 255,
+                        b: 255,
+                    }),
+                },
             },
         },
     ]));
@@ -337,12 +396,12 @@ fn main() {
 
                 prompt_text = prompt_text + &c.to_string()[0..];
                 let mut lock = elements.lock().unwrap();
-                lock.get_mut(1).unwrap().content = prompt_text.clone();
+                lock.get_mut(1).unwrap().element.content = prompt_text.clone();
             }
             KeyCode::Backspace if !prompt_text.is_empty() => {
                 prompt_text = (&prompt_text[0..prompt_text.len() - 1]).to_string();
                 let mut lock = elements.lock().unwrap();
-                lock.get_mut(1).unwrap().content = prompt_text.clone();
+                lock.get_mut(1).unwrap().element.content = prompt_text.clone();
             }
             _ => {}
         }
